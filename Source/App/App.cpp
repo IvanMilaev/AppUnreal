c0d6ee9d0de4b16ca9ae4aee27933f65bd33bf40
milaevivan <milaevivan@protonmail.com>
2021-02-04 18:58:28 +0200

    Initial commit

---
+++ Source/App/App.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "App.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, App, "App" );
---
+++ Source/App/AppGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AppGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class APP_API AAppGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
